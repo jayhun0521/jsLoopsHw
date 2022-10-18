1edcfa8f7e34b9f6fad468fb7195d8bf37ad9951
jayhun0521 <jayhun21@gmail.com>
2022-10-17 22:44:11 -0400

    new push

---
+++ app.js
@@ -0,0 +1,38 @@
+// var myArr = [];
+
+// for (let i = 1; i < 11; i++){
+//     myArr.push(i);
+//     console.log(i);
+// }
+
+
+
+// var myArr = [];
+// let i = 11;
+
+// while (i > 0){
+//     myArr.push(i);
+//     i--;
+//     console.log(i);
+// }
+ 
+
+
+// var myArr = [];
+
+// for (let i = 11; i < 22; i += 2){
+//     myArr.push(i);
+//     console.log(i)
+// }
+
+
+
+
+var myArr = [];
+let i = 17;
+
+while (i > 1){
+    myArr.push(i);
+    i-=2;
+    console.log(i);
+}
